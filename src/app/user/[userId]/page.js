2f90f93ee060e460b6b1ffe4ab94760c0f629c4b
Theo Reeves <theo_25@icloud.com>
2024-08-04 18:07:09 +0100

    added conditional render if they havent filled out a bio to fill that out first before they are shown a profile and if they have then show profile page with name and bio

--- src/app/user/[userId]/page.js
+++ src/app/user/[userId]/page.js
@@ -29,32 +29,67 @@ export default async function UserIdPage() {
     redirect(`/user/${userId}`);
   }
 
-  return (
-    <>
-      <form action={handleSubmit} className="flex flex-col items-center mt-10">
-        <input name="clerk_id" defaultValue={userData.id} hidden></input>
-        <label htmlFor="username">Enter a username:</label>
-        <input
-          className="text-black"
-          name="username"
-          placeholder="Enter your username"
-          required
-        />
-        <label htmlFor="bio">Enter your bio</label>
-        <textarea
-          className="resize text-black"
-          name="bio"
-          required
-          placeholder="Write your bio here!"
-        />
-        <button
-          type="submit"
-          className="flex bg-white rounded text-black items-center text-center
-             w-fit p-1 mt-2 justify-center hover:bg-gray-600 hover:text-white"
+  async function userBio() {
+    const db = dbConnect();
+    const bio = (
+      await db.query(`SELECT * FROM m_users WHERE clerk_id = $1 `, [userId])
+    ).rows;
+    return bio;
+  }
+
+  const Bio = await userBio();
+  const db = dbConnect();
+  const usersData = (
+    await db.query(`SELECT * FROM m_users WHERE clerk_id = $1`, [userId])
+  ).rows;
+
+  if (usersData.length > 0) {
+    return (
+      <main className="flex flex-col items-center">
+        <h1 size={"8"} className="m-2">
+          {userData.username} Profile Page
+        </h1>
+        <div id="profile-info">
+          {Bio.map((item) => (
+            <div key={item.id}>
+              <h2>Bio: {item.bio}</h2>
+            </div>
+          ))}
+        </div>
+      </main>
+    );
+  } else {
+    return (
+      <>
+        <h1>Create your Profile</h1>
+        <form
+          action={handleSubmit}
+          className="flex flex-col items-center mt-10"
         >
-          Create profile
-        </button>
-      </form>
-    </>
-  );
+          <input name="clerk_id" defaultValue={userData.id} hidden></input>
+          <label htmlFor="username">Enter a username:</label>
+          <input
+            className="text-black"
+            name="username"
+            placeholder="Enter your username"
+            required
+          />
+          <label htmlFor="bio">Enter your bio</label>
+          <textarea
+            className="resize text-black"
+            name="bio"
+            required
+            placeholder="Write your bio here!"
+          />
+          <button
+            type="submit"
+            className="flex bg-white rounded text-black items-center text-center
+             w-fit p-1 mt-2 justify-center hover:bg-gray-600 hover:text-white"
+          >
+            Create profile
+          </button>
+        </form>
+      </>
+    );
+  }
 }
