0f3f13c7d376becbb99014280d9210b3431e109b
Jake Darby <designdarby@gmail.com>
2024-08-05 11:01:05 +0100

    dynamic page edits video

--- src/app/movie-page/[movie_id]/page.js
+++ src/app/movie-page/[movie_id]/page.js
@@ -8,16 +8,19 @@ export default async function MoviePageId({ params }) {
   const response = await fetch(
     // `https://api.themoviedb.org/3/movie/${params.movie_id}?api_key=${apiKey}&language=en-US`
 
-   
     `https://api.themoviedb.org/3/movie/${params.movie_id}?api_key=${apiKey}&language=en-US&page=1`
-
   );
   //We had to stringify the data, so we are parsing it back to json
   const data = await response.json();
   // const wrangledData = data.now_playing.results;
 
-  console.log(data);
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${params.movie_id}/videos?api_key=${apiKey}&language=en-US`
+  );
+  const video = (await res.json()).results;
 
+  console.log(video);
+  // console.log(data);
   return (
     <main className="flex min-h-screen flex-col items-center ">
       <h1>Single movie page</h1>
@@ -35,6 +38,26 @@ export default async function MoviePageId({ params }) {
             height={500}
             alt={`Poster for the ${data.original_title} film.`}
           />
+          {video.map((trailer) => {
+            <div className="w-3 h-3 bg-slate-100">
+              <video
+                width="560"
+                height="315"
+                src={`https://api.themoviedb.org/3/movie/${params.movie_id}?api_key=${apiKey}&append_to_response=videos`}
+              ></video>
+              {/* <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${trailer.id}?si=${trailer.key}`}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                referrerpolicy="strict-origin-when-cross-origin"
+                allowfullscreen
+              ></iframe> */}
+              ;
+            </div>;
+          })}
           <Button>
             <Link href={data.homepage}>View film</Link>
           </Button>
--- src/app/movie-page/page.js
+++ src/app/movie-page/page.js
@@ -1,6 +1,7 @@
 // import { Row } from "@radix-ui/themes/src/components/table.jsx";
 import Link from "next/link";
 const apiKey = process.env.API_KEY;
+import Image from "next/image";
 
 export default async function MoviePage() {
   const response = await fetch(
@@ -8,7 +9,9 @@ export default async function MoviePage() {
   );
   //We had to stringify the data, so we are parsing it back to json
   const data = await response.json();
-  const base_url = `https://image.tmdb.org/t/p/w500/`;
+  const base_url = `https://image.tmdb.org/t/p/w500`;
+
+  console.log(data);
 
   //   console.log(data.results);
 
@@ -21,9 +24,11 @@ export default async function MoviePage() {
             <li key={item.id}>
               <Link href={`/movie-page/${item.id}`}>
                 <h1>{item.title}</h1>
-                <image
+                <Image
                   src={`${base_url}${item.poster_path}`}
-                  alt={item.title}
+                  width={500}
+                  height={500}
+                  alt={`Poster for the ${item.title} film.`}
                 />
                 <p>{item.vote_average}</p>
               </Link>
