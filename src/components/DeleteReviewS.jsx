662607cf48b707bf96f8c52ebff07a472d4d02f0
Theo Reeves <theo_25@icloud.com>
2024-08-08 15:16:44 +0100

    working

--- src/components/DeleteReviewS.jsx
+++ src/components/DeleteReviewS.jsx
@@ -5,7 +5,7 @@ import { MdOutlineDeleteOutline } from "react-icons/md";
 import { Button } from "@radix-ui/themes";
 import { AlertDialog, Flex } from "@radix-ui/themes";
 
-export default function deleteButton({ userId, review, del }) {
+export default function deleteButton({ userId, review, params }) {
   async function handleSubmit() {
     "use server";
     const db = dbConnect();
@@ -18,8 +18,8 @@ SET reviews_left = reviews_left - 1
 WHERE clerk_id = $1`,
       [userId]
     );
-    revalidatePath(`/tv-page/${del}`);
-    redirect(`/tv-page/${del}`);
+    revalidatePath(`/tv-page/${params}`);
+    redirect(`/tv-page/${params}`);
   }
 
   return (
